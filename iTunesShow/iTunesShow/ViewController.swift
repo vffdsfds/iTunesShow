0c68b08c40884f05b11b15bf00f9cf1e1991b501
Chen <1582151629@qq.com>
2018-04-23 13:48:38 -0500

    Create a new iOS project

---
+++ iTunesShow/iTunesShow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesShow
+//
+//  Created by Xin Chen on 2018/4/23.
+//  Copyright © 2018年 Xin Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
